178dd03799aaaa963a1243839c829eb4e0b9dad6
umarcor <unai.martinezcorral@ehu.eus>
2019-11-22 22:39:04 +0100

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  transpileDependencies: [
+    "vuetify"
+  ],
+  pluginOptions: {
+    // https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/configuration.html#table-of-contents
+    electronBuilder: {
+      builderOptions: {
+        linux: {
+          target: [ "tar.gz", "7z" ]
+        }
+      }
+    }
+  }
+}
