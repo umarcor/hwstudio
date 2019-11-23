286b6bc64fb8cc51988080131aa0c2e9aad02334
umarcor <unai.martinezcorral@ehu.eus>
2019-11-23 18:21:10 +0100

    fix

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,9 @@ module.exports = {
     // https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/configuration.html#table-of-contents
     electronBuilder: {
       builderOptions: {
-        linux: [ "tar.gz", "7z" ]
+        linux: {
+          target: [ "tar.gz", "7z" ]
+        }
       }
     }
   }
