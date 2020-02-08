ae79fa1d276812d7696cc55e9adba7d4a77917c3
umarcor <unai.martinezcorral@ehu.eus>
2020-02-08 03:45:11 +0100

    add and route NotFound

---
+++ src/components/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+<v-container
+  class="fill-height"
+  fluid
+>
+  <v-row
+    align="center"
+    justify="center"
+  >
+  <v-alert type="error">
+    <strong>404</strong> Page Not Found
+  </v-alert>
+  </v-row>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/Home";
 import Scene from "@/components/Scene";
+import NotFound from "@/components/NotFound";
 
 Vue.use(Router);
 
@@ -25,5 +26,10 @@ export default new Router({
       name: "scene",
       component: Scene
     },
+    {
+      path: "*",
+      name: "notfound",
+      component: NotFound
+    }
   ]
 });
