e6309f596e01ae9837be6a38ed5577a83759d206
umarcor <unai.martinezcorral@ehu.eus>
2020-02-08 11:45:02 +0100

    vuex: enable strict mode to throw errors on mutations outside of mutation handlers

--- src/store.js
+++ src/store.js
@@ -4,6 +4,11 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+
+  // https://vuex.vuejs.org/guide/strict.html
+  strict: process.env.NODE_ENV !== 'production',
+
+  // https://vuex.vuejs.org/guide/state.html
   state: {
     alive: false,
     drawer: false,
@@ -13,8 +18,14 @@ export default new Vuex.Store({
       layers: [true, true, true],
     }
   },
+
+  // https://vuex.vuejs.org/guide/mutations.html
   mutations: {},
+
+  // https://vuex.vuejs.org/guide/actions.html
   actions: {},
+
+  // https://vuex.vuejs.org/guide/getters.html
   getters: {
     isAt: (state) => (name) => {
       return state.route.name === name
