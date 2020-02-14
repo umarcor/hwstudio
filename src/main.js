ccbf99e0a4bedba432912958eb444d7e4f008f98
umarcor <unai.martinezcorral@ehu.eus>
2020-02-14 05:53:15 +0100

    remove src/plugins

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify/lib';
 
 import App from './App.vue';
 import router from '@/router';
 import store from '@/store';
-import vuetify from './plugins/vuetify';
 
-sync(store, router)
+sync(store, router);
+
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify({});
 
 Vue.config.productionTip = false
 
