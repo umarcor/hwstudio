527fb6dfba1367f859f147f26d34716748b41167
umarcor <unai.martinezcorral@ehu.eus>
2021-05-31 08:05:09 +0200

    vue-cli-plugin-electron-builder: use electron-devtools-installer instead of installVueDevtools

--- src/background.js
+++ src/background.js
@@ -9,10 +9,8 @@ import {
   MenuItem
 } from 'electron'
 
-import {
-  createProtocol,
-  installVueDevtools
-} from 'vue-cli-plugin-electron-builder/lib'
+import createProtocol from 'vue-cli-plugin-electron-builder/lib'
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -144,7 +142,7 @@ app.on('ready', async () => {
     // If you are not using Windows 10 dark mode, you may uncomment these lines
     // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
     try {
-      await installVueDevtools()
+      await installExtension(VUEJS_DEVTOOLS)
     } catch (e) {
       console.error('Vue Devtools failed to install:', e.toString())
     }
